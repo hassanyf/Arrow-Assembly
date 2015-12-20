ae1243ed89bcdf75a32df337fe7f78b150464560
hassanyf <mrxortax@gmail.com>
2015-12-20 13:21:56 +0500

    Initial Commit

--- arrow.asm
+++ arrow.asm
@@ -1,6 +1,18 @@
 [org 0x0100]
 
+jmp START
 	
+CLRSCREEN:
+	mov cx, 4000
+	mov ax, 0x0720
+	CLD
+	rep stosw
+	ret
+
+START:
+	mov ax, 0xb800
+	mov es,ax
+	call CLRSCREEN	
 
 mov ax, 0x4c00
 INT 0x21
