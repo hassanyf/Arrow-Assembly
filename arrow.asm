eb50843d5d6fbfb45c5e96cafd5130a80ae8d6b7
hassanyf <mrxortax@gmail.com>
2015-12-20 13:23:48 +0500

    Move arrow to right

--- arrow.asm
+++ arrow.asm
@@ -9,10 +9,34 @@ CLRSCREEN:
 	rep stosw
 	ret
 
+RIGHT:
+	xor di, di
+	mov cx, 80
+here:
+	mov ax, 0x073E
+	push cx
+	mov cx, 60000
+delay:
+	loop delay
+	pop cx
+	dec di
+	dec di
+	push ax
+	mov ax, 0x0720
+	mov word[es:di], ax
+	pop ax
+	inc di
+	inc di
+	CLD
+	stosw
+	loop here
+	ret
+
 START:
 	mov ax, 0xb800
 	mov es,ax
 	call CLRSCREEN
+	call RIGHT
 
 mov ax, 0x4c00
 INT 0x21
